f22629d75fc26e5200936484f92c5bd54e4cc6a8
tabhan <tabhan@aaxiscommerce.com>
2018-09-19 17:02:22 +0800

    Init symfony form

---
+++ src/Controller/FormExampleController.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Controller;
+use App\Entity\Product;
+use App\Form\Type\ProductType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+class FormExampleController extends Controller
+{
+    /**
+     * @Route("/", name="form_example")
+     */
+    public function formExampleAction(Request $request)
+    {
+        $form = $this->createForm(ProductType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $product = $form->getData();
+            $em->persist($product);
+            $em->flush();
+            return $this->redirectToRoute('form_example');
+        }
+        return $this->render('/form/product.html.twig', [
+            'productForm' => $form->createView()
+        ]);
+    }
+    /**
+     * @Route("/{product}", name="form_edit_example")
+     */
+    public function formEditExampleAction(Request $request, Product $product)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            return $this->redirectToRoute('form_edit_example', ['product'=>$product->getId()]);
+        }
+        return $this->render('/form/product.html.twig', [
+            'productForm' => $form->createView()
+        ]);
+    }
+}
---
+++ src/Form/Type/ProductType.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Form\Type;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('availableFrom', DateTimeType::class)
+            ->add('description', TextareaType::class)
+            ->add('save', SubmitType::class)
+        ;
+    }
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => 'App\Entity\Product'
+        ]);
+    }
+}
